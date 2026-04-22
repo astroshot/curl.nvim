2b5fc0d231cb239e784c9081b7e98339127daa63
Shockwave <lzhdev@yeah.net>
2026-04-22 15:33:18 +0800

    Sub: Try to drop deprecated nvim_buf_add_highlight
    Body:
    
    ==== End ====

--- lua/curl/output_parser.lua
+++ lua/curl/output_parser.lua
@@ -7,71 +7,71 @@ local notify = require("curl.notifications")
 ---@param unformatted_json string
 ---@return table
 local function run_jq(unformatted_json)
-	local result = {}
+  local result = {}
 
-	Job:new({
-		command = "jq",
-		args = { "." },
-		writer = unformatted_json,
-		on_stdout = function(_, line)
-			table.insert(result, line)
-		end,
-		on_exit = function(_, return_val)
-			if return_val ~= 0 then
-				vim.schedule(function()
-					notify.error("Failed to parse JSON")
-				end)
+  Job:new({
+    command = "jq",
+    args = { "." },
+    writer = unformatted_json,
+    on_stdout = function(_, line)
+      table.insert(result, line)
+    end,
+    on_exit = function(_, return_val)
+      if return_val ~= 0 then
+        vim.schedule(function()
+          notify.error("Failed to parse JSON")
+        end)
 
-				result = { unformatted_json }
-			end
-		end,
-	}):sync()
+        result = { unformatted_json }
+      end
+    end,
+  }):sync()
 
-	return result
+  return result
 end
 
 local function trim(s)
-	local from = s:match("^%s*()")
-	return from > #s and "" or s:match(".*%S", from)
+  local from = s:match("^%s*()")
+  return from > #s and "" or s:match(".*%S", from)
 end
 
 local function is_json_start(line)
-	return line:match("^[%[%{]") ~= nil
+  return line:match("^[%[%{]") ~= nil
 end
 
 local function extract_json(output_lines)
-	local header_lines = {}
-	local json_string = nil
+  local header_lines = {}
+  local json_string = nil
 
-	for _, line in ipairs(output_lines) do
-		local trimmed_line = trim(line)
-		if is_json_start(trimmed_line) then
-			json_string = trimmed_line
-			break
-		end
-		table.insert(header_lines, trimmed_line)
-	end
+  for _, line in ipairs(output_lines) do
+    local trimmed_line = trim(line)
+    if is_json_start(trimmed_line) then
+      json_string = trimmed_line
+      break
+    end
+    table.insert(header_lines, trimmed_line)
+  end
 
-	return header_lines, json_string
+  return header_lines, json_string
 end
 
 ---
 ---@param curl_standard_out string
 ---@return table
 M.parse_curl_output = function(curl_standard_out)
-	if is_json_start(curl_standard_out) then
-		return run_jq(curl_standard_out)
-	end
+  if is_json_start(curl_standard_out) then
+    return run_jq(curl_standard_out)
+  end
 
-	local output_table = vim.split(curl_standard_out, "\r")
-	local header_lines, json_string = extract_json(output_table)
+  local output_table = vim.split(curl_standard_out, "\r")
+  local header_lines, json_string = extract_json(output_table)
 
-	if json_string == nil then
-		return output_table
-	end
+  if json_string == nil then
+    return output_table
+  end
 
-	local json_lines = run_jq(json_string)
-	table.move(json_lines, 1, #json_lines, #header_lines + 1, header_lines)
-	return header_lines
+  local json_lines = run_jq(json_string)
+  table.move(json_lines, 1, #json_lines, #header_lines + 1, header_lines)
+  return header_lines
 end
 return M
--- lua/curl/treesitter.lua
+++ lua/curl/treesitter.lua
@@ -1,63 +1,67 @@
 local M = {}
 
 M.add_treesitter_highlighting = function()
-	-- Custom highlights for curl-specific elements
-	local query = vim.treesitter.query.parse(
-		"bash",
-		[[
-      ;; Highlight the 'curl' command
-      ((command_name) @curl (#eq? @curl "curl"))
+  -- Custom highlights for curl-specific elements
+  local query = vim.treesitter.query.parse(
+    "bash",
+    [[
+    ;; Highlight the 'curl' command
+    ((command_name) @curl (#eq? @curl "curl"))
 
-      ;; Highlight curl options
-      ((word) @curl_option (#match? @curl_option "^-"))
+    ;; Highlight curl options
+    ((word) @curl_option (#match? @curl_option "^-"))
 
-      ;; Highlight HTTP methods
-      ((word) @http_method (#match? @http_method "^(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)$"))
+    ;; Highlight HTTP methods
+    ((word) @http_method (#match? @http_method "^(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)$"))
 
-      ;; Highlight URLs
-      ((word) @url (#match? @url "^https?://"))
+    ;; Highlight URLs
+    ((word) @url (#match? @url "^https?://"))
 
-      ;; Highlight variables
-      ;; FIXME: will have many false-positives but idk how to do "\v---.{-}\=.+$" 
-      ((command) @custom_var (#match? @custom_var "^---.+$"))
+    ;; Highlight variables
+    ;; FIXME: will have many false-positives but idk how to do "\v---.{-}\=.+$" 
+    ((command) @custom_var (#match? @custom_var "^---.+$"))
     ]]
-	)
+  )
 
-	local bufnr = 0
-	local parser = vim.treesitter.get_parser(bufnr, "bash")
-	local tree = parser:parse()[1]
-	local root = tree:root()
+  local bufnr = 0
+  local parser = vim.treesitter.get_parser(bufnr, "bash")
+  local tree = parser:parse()[1]
+  local root = tree:root()
 
-	for id, node in query:iter_captures(root, bufnr, 0, -1) do
-		local name = query.captures[id]
-		local start_row, start_col, end_row, end_col = node:range()
+  for id, node in query:iter_captures(root, bufnr, 0, -1) do
+    local name = query.captures[id]
+    local start_row, start_col, end_row, end_col = node:range()
 
-		if name == "curl" then
-			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlKeyword", start_row, start_col, end_col)
-		elseif name == "curl_option" or name == "http_method" then
-			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlFunction", start_row, start_col, end_col)
-		elseif name == "custom_var" then
-			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlVariable", start_row, start_col, end_col)
-		elseif name == "url" then
-			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlUrl", start_row, start_col, end_col)
-		end
-	end
+    if name == "curl" then
+      -- vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlKeyword", start_row, start_col, end_col)
+      vim.hl.range(0, -1, "CurlCommandHighlight", {start_row, start_col}, {start_row, end_col}, {})
+    elseif name == "curl_option" or name == "http_method" then
+      -- vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlFunction", start_row, start_col, end_col)
+      vim.hl.range(0, -1, "CurlFunction", {start_row, start_col}, {start_row, end_col}, {})
+    elseif name == "custom_var" then
+      -- vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlVariable", start_row, start_col, end_col)
+      vim.hl.range(0, -1, "CurlVariable", {start_row, start_col}, {start_row, end_col}, {})
+    elseif name == "url" then
+      -- vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlUrl", start_row, start_col, end_col)
+      vim.hl.range(0, -1, "CurlUrl", {start_row, start_col}, {start_row, end_col}, {})
+    end
+  end
 
-	vim.api.nvim_set_hl(0, "CurlUrl", {
-		link = "@text",
-	})
+  vim.api.nvim_set_hl(0, "CurlUrl", {
+    link = "@text",
+  })
 
-	vim.api.nvim_set_hl(0, "CurlFunction", {
-		link = "@function",
-	})
+  vim.api.nvim_set_hl(0, "CurlFunction", {
+    link = "@function",
+  })
 
-	vim.api.nvim_set_hl(0, "CurlVariable", {
-		link = "@variable",
-	})
+  vim.api.nvim_set_hl(0, "CurlVariable", {
+    link = "@variable",
+  })
 
-	vim.api.nvim_set_hl(0, "CurlKeyword", {
-		link = "@operator",
-	})
+  vim.api.nvim_set_hl(0, "CurlKeyword", {
+    link = "@operator",
+  })
 end
 
 return M
