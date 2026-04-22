fb961b92c63ac1061be1d9ec8a1d7527de399b6b
Shockwave <lzhdev@yeah.net>
2026-04-22 22:01:24 +0800

    Fix cache

--- lua/curl/cache.lua
+++ lua/curl/cache.lua
@@ -1,90 +1,99 @@
 local M = {}
 local notify = require("curl.notifications")
 
+local exists = function(file)
+  if vim.uv.fs_stat(file) then
+    return true
+  end
+  return false
+end
+
 ---comment
 ---@param global boolean
 ---@return string
 local function get_custom_dir(global)
-	if global then
-		return "custom"
-	else
-		local workspace_path = vim.fn.getcwd()
-		local unique = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
-		return "scopedcustom/" .. unique
-	end
+  if global then
+    return "custom"
+  else
+    local workspace_path = vim.fn.getcwd()
+    local unique = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
+    return "scopedcustom/" .. unique
+  end
 end
 
 local curl_cache_dir = function(custom_dir)
   local cache_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "curl_cache")
 
-	if custom_dir then
+  if custom_dir then
     cache_dir = vim.fs.joinpath(cache_dir, custom_dir)
-	end
+  end
 
-	if vim.fn.mkdir(cache_dir:absolute(), "p") ~= 1 then
-		notify.error("Error creating collection: could not create directory : " .. cache_dir:absolute())
-	end
+  local abs_cache_dir = vim.fs.abspath(cache_dir)
+  if vim.fn.mkdir(abs_cache_dir, "p") ~= 1 then
+    notify.error("Error creating collection: could not create directory : " .. abs_cache_dir)
+  end
 
-	return cache_dir
+  return cache_dir
 end
 
 ---@param filename string
 ---@return string
 M.load_custom_command_file = function(filename, global)
-	local custom_dir = get_custom_dir(global)
-	local cache_dir = curl_cache_dir(custom_dir)
+  local custom_dir = get_custom_dir(global)
+  local cache_dir = curl_cache_dir(custom_dir)
 
-	local curl_filename = filename .. ".curl"
+  local curl_filename = filename .. ".curl"
   local custom_cache_dir = vim.fs.joinpath(cache_dir, curl_filename)
-	return custom_cache_dir:absolute()
+  return vim.fs.abspath(custom_cache_dir)
 end
 
 ---@return string
 M.load_global_command_file = function()
-	local cache_dir = curl_cache_dir()
+  local cache_dir = curl_cache_dir()
 
-	local global_cache_file = vim.fs.joinpath(cache_dir, "global.curl")
-	return global_cache_file:absolute()
+  local global_cache_file = vim.fs.joinpath(cache_dir, "global.curl")
+  return vim.fs.abspath(global_cache_file)
 end
 
 ---@return string
 M.load_command_file = function()
-	local workspace_path = vim.fn.getcwd()
-	local cache_dir = curl_cache_dir()
+  local workspace_path = vim.fn.getcwd()
+  local cache_dir = curl_cache_dir()
 
-	local unique_id = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
-	local new_file_name = unique_id .. ".curl"
+  local unique_id = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
+  local new_file_name = unique_id .. ".curl"
 
-	local old_cache_file = vim.fs.joinpath(cache_dir, vim.fn.sha256(workspace_path))
-	local new_cache_file = vim.fs.joinpath(cache_dir, new_file_name)
+  local old_cache_file = vim.fs.joinpath(cache_dir, vim.fn.sha256(workspace_path))
+  local new_cache_file = vim.fs.joinpath(cache_dir, new_file_name)
 
-	if old_cache_file:exists() then
-		if not new_cache_file:exists() then
-			old_cache_file:rename({ new_name = new_cache_file:absolute() })
-		else
-			local archive_file = vim.fs.joinpath(cache_dir, vim.fn.sha256(workspace_path), ".archive")
-			old_cache_file:rename({ new_name = archive_file:absolute() })
-		end
-	end
+  if exists(old_cache_file) then
+    if not exists(new_cache_file) then
+      old_cache_file:rename({ new_name = vim.fs.abspath(new_cache_file) })
+    else
+      local archive_file = vim.fs.joinpath(cache_dir, vim.fn.sha256(workspace_path), ".archive")
+      old_cache_file:rename({ new_name = vim.fs.abspath(archive_file) })
+    end
+  end
 
-	return new_cache_file:absolute()
+  return vim.fs.abspath(new_cache_file)
 end
 
 ---@param global boolean set to true to search global scorep
 ---@return table Table of collection in the given scope
 M.get_collections = function(global)
-	local collection_dir = curl_cache_dir(get_custom_dir(global)):absolute()
+  local collection_dir = curl_cache_dir(get_custom_dir(global))
+  collection_dir = vim.fs.abspath(collection_dir)
 
-	local scan = require("curl.scandir")
+  local scan = require("curl.scandir")
 
-	local filepaths = scan.scan_dir(collection_dir, { depth = 1 }) ---@type string[]
+  local filepaths = scan.scan_dir(collection_dir, { depth = 1 }) ---@type string[]
 
-	local files = {}
-	for _, filepath in ipairs(filepaths) do
-		table.insert(files, vim.fn.fnamemodify(filepath, ":t:r"))
-	end
+  local files = {}
+  for _, filepath in ipairs(filepaths) do
+    table.insert(files, vim.fn.fnamemodify(filepath, ":t:r"))
+  end
 
-	return files
+  return files
 end
 
 return M
